ad5c0f7fd8510277fae847dd37c03fb9a9138bc1
Xongile Nghatsane <xnghatsane@gmail.com>
2019-03-06 13:51:29 +0200

    Lab 2

---
+++ index.js
@@ -0,0 +1,14 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require('./mainRoutes')
+mainRouter.get('/', function (req, res) {
+    res.send('Hello World');
+});
+mainRouter.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+app.use(mainRouter);
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
